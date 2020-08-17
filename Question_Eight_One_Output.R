4d8a93dc35b3e8380ca116c47cbd5328088fb98b
TesiGahiga <tesigahiga@gmail.com>
2020-08-17 16:07:16 +0100

    Add files via upload

---
+++ Question_Eight_One_Output.R
@@ -0,0 +1,12 @@
+
+Question 8
+8.1
+At initial condtions:
+  dS/dt= k2[ES] - k1[S][E],                    [S](0) = S0
+
+dE/dt = (k2 + k3)[ES] - k1[S][E],              [E](0) = E0
+
+dES/dt= k1[S][E] - (k2 + k3)[ES],              [SE](0) = 0
+
+dP/dt= k3[ES],                                 [P](0) = 0.
+
